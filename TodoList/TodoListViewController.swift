a236e97d0ef9f1d079a8ced89f2267c55e2338b0
JaneJung <piaojane@daum.net>
2021-02-09 10:57:26 +0900

    UI textField,addButton,isToday Layout

--- TodoList/TodoListViewController.swift
+++ TodoList/TodoListViewController.swift
@@ -23,20 +23,20 @@ class TodoListViewController: UIViewController {
         //[x] TODO: 데이터 불러오기
         todoListViewModel.loadTasks()
         
-        let todo = TodoManager.shared.createTodo(detail: "11", isToday: true)
-        Storage.saveTodo(todo, fileName: "test.json")
+//        let todo = TodoManager.shared.createTodo(detail: "11", isToday: true)
+//        Storage.saveTodo(todo, fileName: "test.json")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let todo = Storage.restoreTodo("test.json")
-        print("---> restore from disk: \(todo)")
+//        let todo = Storage.restoreTodo("test.json")
+//        print("---> restore from disk: \(todo)")
     }
     
     @IBAction func isTodayButtonTapped(_ sender: Any) {
-        // TODO: 투데이 버튼 토글 작업
-        
+        // [x]  TODO: 투데이 버튼 토글 작업
+        isTodayButton.isSelected = !isTodayButton.isSelected
     }
     
     @IBAction func addTaskButtonTapped(_ sender: Any) {
